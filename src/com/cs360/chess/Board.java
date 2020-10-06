8403b70ca89fd2afa80abc04138b26c064fb5734
NJDaeger <noahdaeger@hotmail.com>
2020-10-06 00:50:28 -0400

    Create some very basic methods for the board and game class. Start documenting some of the code. Preparing for the start of the GUI drawing.

--- src/com/cs360/chess/Board.java
+++ src/com/cs360/chess/Board.java
@@ -2,10 +2,42 @@ package com.cs360.chess;
 
 import com.cs360.chess.piece.Piece;
 
+/**
+ * The grid of pieces which can be manipulated. This can represent a primary board, or a future board depending on its context.
+ */
 public class Board {
 
     private Piece[][] board;
-
-
-
+    
+    /**
+     * Creates a new board instance with a fresh set of pieces
+     */
+    public Board() {
+        //todo generate a prepopulated new chess game board
+    }
+    
+    /**
+     * Creates a new board based on a previous board.
+     * @param parent
+     */
+    public Board(Board parent) {
+    
+    }
+    
+    /**
+     * Moves a piece from x,y to newX, newY. Top left is 0, 0, bottom right is 9, 9
+     * @param x The X position of the piece
+     * @param y The Y position of the piece
+     * @param newX The newX position of the piece
+     * @param newY The newY position of the piece
+     * @return The piece which was kicked out of the new spot, or null if there was no piece occupying newX, newY
+     */
+    public Piece movePiece(int x, int y, int newX, int newY) {
+        if (x > 9 || y > 9 || newX > 9 || newY > 9) throw new RuntimeException("Piece out of bounds");
+        Piece oldPiece = board[newX][newY];
+        board[newX][newY] = board[x][y];
+        board[x][y] = null;
+        return oldPiece;
+    }
+    
 }
--- src/com/cs360/chess/Game.java
+++ src/com/cs360/chess/Game.java
@@ -1,4 +1,14 @@
 package com.cs360.chess;
 
+/**
+ * The Game class is the primary controller behind the whole game. It manages the Minimax tree and the primary board.
+ * This will also serve as the game "instance" object, which can be saved to a file at any given time during a game.
+ *
+ * //todo any additional attributes should be described up here.
+ */
 public class Game {
+    
+    private Board primaryBoard;
+    //todo handle minimax in this class.
+    
 }
