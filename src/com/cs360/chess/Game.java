6469285c1c545271cd64af300227fa29881aa64f
NJDaeger <noahdaeger@hotmail.com>
2020-11-06 00:15:53 -0500

    Add some logic and start a minimax implementation

--- src/com/cs360/chess/Game.java
+++ src/com/cs360/chess/Game.java
@@ -1,22 +1,62 @@
 package com.cs360.chess;
 
+import com.cs360.chess.ui.Minimax;
+
+import java.io.Serializable;
+import java.util.Stack;
+
 /**
  * The Game class is the primary controller behind the whole game. It manages the Minimax tree and the primary board.
  * This will also serve as the game "instance" object, which can be saved to a file at any given time during a game.
  *
  * //todo any additional attributes should be described up here.
  */
-public class Game {
-    
-    private Board primaryBoard;
+public class Game implements Serializable {
+
+    private int depth;
+    private Board currentBoard;
+    private Minimax currentTree;
+    private Stack<Board> undoStack;
+    private Stack<Board> redoStack;
     //todo handle minimax in this class.
 
     public Game() {
-        primaryBoard = new Board();
+        //The last move we just did
+        this.undoStack = new Stack<>();
+        //The last move we just undid
+        this.redoStack = new Stack<>();
+        this.currentBoard = new Board();
+        this.currentTree = new Minimax(currentBoard, depth);
     }
 
     public Board getCurrentBoard() {
-        return primaryBoard;
+        return currentBoard;
     }
-    
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    /**
+     * Undoes the current board to the last board state
+     * @return The board before being undone.
+     */
+    public Board undo() {
+        Board lastMove = undoStack.pop();
+        redoStack.push(currentBoard);
+        this.currentBoard = lastMove;
+        return redoStack.peek();
+    }
+
+    /**
+     * Redoes the current board to the previous state
+     * @return The board before being redone
+     */
+    public Board redo() {
+        Board nextMove = redoStack.pop();
+        undoStack.push(currentBoard);
+        this.currentBoard = nextMove;
+        return undoStack.peek();
+    }
+
 }
---
+++ src/com/cs360/chess/ui/Minimax.java
@@ -0,0 +1,19 @@
+package com.cs360.chess.ui;
+
+import com.cs360.chess.Board;
+
+public class Minimax {
+
+    private Board board;
+    private int depth;
+
+    public Minimax(Board board, int depth) {
+        this.board = board;
+        this.depth = depth;
+    }
+
+    private class Node {
+
+    }
+
+}
